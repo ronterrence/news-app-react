f326d15b1d5843725fc0e9c2dc2b00b7edaad4af
ron <ronterrence@gmail.com>
2026-07-07 06:36:43 +0200

    news rm image

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -25,41 +25,13 @@ function NewsList({ articles }) {
     <div className="news-list">
       {articles.map((article) => (
         <article key={article.id} className="news-item-card">
-          {article.imageUrl ? (
-            <img
-              className="news-image"
-              src={article.imageUrl}
-              alt={article.title}
-              loading="lazy"
-            />
-          ) : (
-            <div className="news-image news-image-placeholder">No image</div>
-          )}
-
           <div className="news-item-content">
+            <h4 className="news-title">{article.title}</h4>
+
             <div className="news-meta-row">
               <span className="news-source">{article.source}</span>
               <span className="news-date">{formatPublishedDate(article.publishedAt)}</span>
             </div>
-
-            <h4 className="news-title">{article.title}</h4>
-
-            <p className="news-description">
-              {article.description || "No description available for this article."}
-            </p>
-
-            {article.url ? (
-              <a
-                className="news-link"
-                href={article.url}
-                target="_blank"
-                rel="noreferrer"
-              >
-                Read article
-              </a>
-            ) : (
-              <span className="news-link news-link-disabled">Link unavailable</span>
-            )}
           </div>
         </article>
       ))}
@@ -68,4 +40,3 @@ function NewsList({ articles }) {
 }
 
 export default NewsList;
-
